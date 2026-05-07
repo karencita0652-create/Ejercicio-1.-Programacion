e20548a57b70d544f01767065700e8eef9b8dbbc
karencita0652-create <karencita0652@gmail.com>
2026-05-06 21:19:28 -0500

    Add error handling for Cliente instantiation

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Proyecto funcionando")
+from modelos.cliente import Cliente
+
+try:
+
+    cliente1 = Cliente("Ana", "correo_malo", 20)
+
+except Exception as e:
+
+    print("Ocurrió un error:", e)
+
+print("El programa sigue funcionando")
