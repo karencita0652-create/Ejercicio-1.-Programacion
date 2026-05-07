a67d6dda7c5487bf157ff61beec524d583cf86da
karencita0652-create <karencita0652@gmail.com>
2026-05-06 21:23:22 -0500

    Update client details and handle service reservation

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from modelos.cliente import Cliente
+from modelos.servicios import ReservaSala
 from utils.logger import registrar_error
 
 try:
 
-    cliente1 = Cliente("Ana", "correo_malo", 20)
+    cliente1 = Cliente("Juan", "juan@gmail.com", 25)
+
+    servicio1 = ReservaSala("Sala VIP", 100)
+
+    print(cliente1.nombre)
+
+    print(servicio1.mostrar_servicio())
 
 except Exception as e:
 
-    print("Ocurrió un error:", e)
+    print("Error:", e)
 
     registrar_error(str(e))
-
-print("El programa sigue funcionando")
