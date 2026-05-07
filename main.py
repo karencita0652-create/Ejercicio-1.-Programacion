24e2c6989124a5cf3e67ab570ab5791fab8366c5
karencita0652-create <karencita0652@gmail.com>
2026-05-06 21:28:19 -0500

    Update client initialization and error handling

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from modelos.servicios import ReservaSala
 from modelos.reserva import Reserva
 from utils.logger import registrar_error
 
+
+print("INICIO DEL SISTEMA")
+
+
+# CLIENTE 1 CORRECTO
 try:
 
-    cliente1 = Cliente("Ana", "correo_malo", 15)
+    cliente1 = Cliente("Juan", "juan@gmail.com", 25)
 
     servicio1 = ReservaSala("Sala VIP", 100)
 
@@ -13,10 +18,37 @@ try:
 
     print(reserva1.mostrar_reserva())
 
+    print(reserva1.confirmar())
+
 except Exception as e:
 
     print("Error:", e)
 
     registrar_error(str(e))
 
-print("El sistema sigue funcionando")
+
+# CLIENTE 2 CON CORREO MALO
+try:
+
+    cliente2 = Cliente("Ana", "correo_malo", 22)
+
+except Exception as e:
+
+    print("Error:", e)
+
+    registrar_error(str(e))
+
+
+# CLIENTE 3 MENOR DE EDAD
+try:
+
+    cliente3 = Cliente("Pedro", "pedro@gmail.com", 15)
+
+except Exception as e:
+
+    print("Error:", e)
+
+    registrar_error(str(e))
+
+
+print("EL SISTEMA SIGUE FUNCIONANDO")
