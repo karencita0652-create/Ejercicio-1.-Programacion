5eea84f9bf79b2e392e31c4755b50644deed92e0
karencita0652-create <karencita0652@gmail.com>
2026-05-06 21:22:40 -0500

    Add new service classes for ReservaSala, AlquilerEquipo, and Asesoria

--- modelos/servicios.py
+++ modelos/servicios.py
@@ -4,3 +4,24 @@ class Servicio:
 
         self.nombre = nombre
         self.precio = precio
+
+
+class ReservaSala(Servicio):
+
+    def mostrar_servicio(self):
+
+        return f"Reserva de sala: {self.nombre}"
+
+
+class AlquilerEquipo(Servicio):
+
+    def mostrar_servicio(self):
+
+        return f"Alquiler de equipo: {self.nombre}"
+
+
+class Asesoria(Servicio):
+
+    def mostrar_servicio(self):
+
+        return f"Asesoría: {self.nombre}"
